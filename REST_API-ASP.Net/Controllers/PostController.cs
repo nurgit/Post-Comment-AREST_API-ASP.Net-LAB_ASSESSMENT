9ae8275009e3b13c3c05f27c313353cd688a2b19
Nur Mohammad <16-33013-3@student.aiub.edu>
2020-12-18 20:22:16 +0600

    routing , name Routing

--- REST_API-ASP.Net/Controllers/PostController.cs
+++ REST_API-ASP.Net/Controllers/PostController.cs
@@ -10,13 +10,17 @@ using System.Web.Http;
 
 namespace REST_API_ASP.Net.Controllers
 {
-    public class PostsController : ApiController
+    [RoutePrefix("api/posts")]
+    public class PostController : ApiController
     {
         ComentRepository postRepository = new ComentRepository();
+        [Route("")]
         public IHttpActionResult Get()
         {
             return Ok(postRepository.GetAll());
         }
+
+        [Route("{id}", Name ="GetPostById")]//nothing...just giving a name to this route for next time use
         public IHttpActionResult Get(int id)
         {
             var post = postRepository.Get(id);
@@ -27,12 +31,15 @@ namespace REST_API_ASP.Net.Controllers
             return Ok(postRepository.Get(id));
         }
 
+        [Route("")]
         public IHttpActionResult Post(Post post)
         {
-
+            string uri = Url.Link("GetPostById", new { id=post.PostId});
             postRepository.Insert(post);
-            return Created("api/posts/" + post.PostId, post);
+            return Created(uri, post);
         }
+
+        [Route("")]
         public IHttpActionResult Put([FromUri] int id,[FromBody]Post post)
         {
             post.PostId = id;
@@ -40,6 +47,7 @@ namespace REST_API_ASP.Net.Controllers
             return Ok(post);
         }
 
+        [Route("{id}")]
         public IHttpActionResult Delete(int id)
         {
 
